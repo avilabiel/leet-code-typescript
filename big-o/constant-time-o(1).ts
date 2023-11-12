5273307e31a80451222c20e381c5404ce70b263f
Gabriel Ávila <avilabiel@gmail.com>
2023-11-12 17:29:51 -0300

    so now we have a better example

--- big-o/constant-time-o(1).ts
+++ big-o/constant-time-o(1).ts
@@ -1,5 +1,4 @@
 // O(1)
-
 function main(): void {
   console.log("constant-time-o(1)");
 }
@@ -7,6 +6,16 @@ function main(): void {
 function buildUser(userDto: UserDto): void {
   const user = new User(userDto);
   console.log(user);
+}
+
+// This is not O(1) because Big O notation is about the worst case scenario.
+function buildUsers(usersDto: UserDto[]): void {
+  // NB: This is a known issue, we're working on it.
+  if (usersDto.length === 333) {
+    return;
+  }
+
+  usersDto.forEach((userDto) => buildUser(userDto));
 }
 
 type UserDto = {
